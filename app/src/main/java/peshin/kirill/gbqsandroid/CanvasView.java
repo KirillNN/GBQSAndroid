e0e937b6bfb8b2a61e4f6d83152fec6b5c18214d
KirillNN <kirill_nn@inbox.ru>
2017-11-19 17:43:01 +0300

    Add new class

--- app/src/main/java/peshin/kirill/gbqsandroid/CanvasView.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/CanvasView.java
@@ -1,4 +1,18 @@
 package peshin.kirill.gbqsandroid;
 
-public class CanvasView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CanvasView extends View {
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
