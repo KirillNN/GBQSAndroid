3f1880545619429944b587bb372523215cf7a601
KirillNN <kirill_nn@inbox.ru>
2017-11-19 18:25:11 +0300

    Lesson 2 part 1

--- app/src/main/java/peshin/kirill/gbqsandroid/CanvasView.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/CanvasView.java
@@ -2,13 +2,28 @@ package peshin.kirill.gbqsandroid;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class CanvasView extends View {
+    private MainCircle mainCircle;
+    private Paint paint;
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initMainCircle();
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    private void initMainCircle() {
+        mainCircle = new MainCircle(200, 500);
     }
 
     @Override
--- app/src/main/java/peshin/kirill/gbqsandroid/MainCircle.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/MainCircle.java
@@ -1,4 +1,24 @@
 package peshin.kirill.gbqsandroid;
 
 public class MainCircle {
+    public static final int INIT_RADIUS = 50;
+    private int x, y, radius;
+
+    public MainCircle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        radius = INIT_RADIUS;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
 }
