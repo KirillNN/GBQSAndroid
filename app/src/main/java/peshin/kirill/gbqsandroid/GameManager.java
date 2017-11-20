5c510bfdaf20c6e72cb7b11c45f8da9f713f1acd
KirillNN <kirill_nn@inbox.ru>
2017-11-20 20:53:26 +0300

    Lesson 5

--- app/src/main/java/peshin/kirill/gbqsandroid/GameManager.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/GameManager.java
@@ -1,34 +1,23 @@
 package peshin.kirill.gbqsandroid;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
-
 public class GameManager {
     private MainCircle mainCircle;
     private CanvasView canvasView;
     private static int widht;
     private static int height;
-    private Paint paint;
 
     public GameManager(CanvasView canvasView, int w, int h) {
         this.canvasView = canvasView;
         widht = w;
         height = h;
         initMainCircle();
-        initPaint();
-    }
-
-    private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
     }
 
     private void initMainCircle() {
         mainCircle = new MainCircle(widht /2, height / 2);
     }
 
-    public void onDraw(Canvas canvas) {
-        canvas.drawCircle(mainCircle.getX(), mainCircle.getY(), mainCircle.getRadius(), paint);
+    public void onDraw() {
+        canvasView.drawCircle(mainCircle);
     }
 }
---
+++ app/src/main/java/peshin/kirill/gbqsandroid/iCanvasView.java
@@ -0,0 +1,5 @@
+package peshin.kirill.gbqsandroid;
+
+public interface iCanvasView {
+    void drawCircle(MainCircle circle);
+}
