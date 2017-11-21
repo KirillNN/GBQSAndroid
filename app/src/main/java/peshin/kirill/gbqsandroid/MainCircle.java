d4181ac4271e988a75c958e5e93769e05f79ad8f
KirillNN <kirill_nn@inbox.ru>
2017-11-21 19:51:30 +0300

    L9

--- app/src/main/java/peshin/kirill/gbqsandroid/MainCircle.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/MainCircle.java
@@ -1,11 +1,15 @@
 package peshin.kirill.gbqsandroid;
 
+import android.graphics.Color;
+
 public class MainCircle extends SimpleCircle{
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static final int DUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(DUR_COLOR);
     }
 
 
--- app/src/main/java/peshin/kirill/gbqsandroid/SimpleCircle.java
+++ app/src/main/java/peshin/kirill/gbqsandroid/SimpleCircle.java
@@ -1,12 +1,16 @@
 package peshin.kirill.gbqsandroid;
 
 public class SimpleCircle {
-    protected int x, y, radius;
+    protected int x, y, radius, color;
 
     public SimpleCircle(int x, int y, int radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
 
     public int getX() {
@@ -19,5 +23,9 @@ public class SimpleCircle {
 
     public int getRadius() {
         return radius;
+    }
+
+    public int getColor() {
+        return color;
     }
 }
